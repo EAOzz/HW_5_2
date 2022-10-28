20d6d477880fb708de42730095df36691eabb7e6
EAOzz <katrinkaya86@gmail.com>
2022-10-28 22:49:25 +0300

    Create repository on GitHub

---
+++ example.js
@@ -0,0 +1,4 @@
+let monthOfBirth = 5; // Привсоен месяц рождения, тип данных number
+console.log(typeof monthOfBirth); // Вывод на экран типа данных
+monthOfBirth = "May"; // Измене тип данных с number на string
+console.log(typeof monthOfBirth); // Вывод на экран типа данных
